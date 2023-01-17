5dd3b5f0f9a9ec8c97d770597a042483bd9223bd
unknown <leandroalonso2000@gmail.comgit config --global user.email leandroalonso2000@gmail.comgit config --global user.email leandroalonso2000@gmail.comgit config --global user.username Leandro>
2023-01-17 19:02:44 -0300

    avance HzNav

---
+++ clubapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import HzNav from './components/HzNav';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HzNav></HzNav>
+    </div>
+  );
+}
+
+export default App;
---
+++ clubapp/src/components/HzNav.js
@@ -0,0 +1,18 @@
+import './HzNav.css'
+
+const HzNav = () => {
+  return (
+    <>
+        <div className='container' >
+            <div className='top-box' >
+                <div className='container-logo-app' >
+                    <p>Logo App</p>
+                </div>
+            </div>
+            <div className='options-box' ></div>
+        </div>
+    </>
+  );
+}
+
+export default HzNav
