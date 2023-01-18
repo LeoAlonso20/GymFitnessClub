5dba6113312f23d49aa1be5e4b9804839ad51189
unknown <leandroalonso2000@gmail.comgit config --global user.email leandroalonso2000@gmail.comgit config --global user.email leandroalonso2000@gmail.comgit config --global user.username Leandro>
2023-01-18 02:48:05 -0300

    HzBar avanzado

--- clubapp/src/components/HzNav/HzNav.js
+++ clubapp/src/components/HzNav/HzNav.js
@@ -1,16 +1,37 @@
 import './HzNav.css'
+import {Animated} from "react-animated-css";
+import { useState } from 'react';
+
 
 const HzNav = () => {
+
+  const [open, setOpen] = useState(true)
+  
+  const handleClickButton = () => {
+    if( open ) setOpen(() => false)
+    else setOpen(() => true)
+  }
+
   return (
     <>
-        <div className='container' >
+        <div className={`container ${open ? "container-close" : ""}`} >
             <div className='top-box' >
                 <div className='container-logo-app' >
-                    <p>Logo App</p>
+                    {open
+                        ? <p>A</p>
+                        : <Animated animationIn='fadeIn' animationInDuration={500}><p>Logo App</p></Animated>
+                    }
                 </div>
             </div>
             <div className='options-box' ></div>
         </div>
+        <button type='button' className={`button-bar ${open ? "button-bar-close" : ""}`} onClick={handleClickButton} >
+          {open
+                ? <i className="fa-solid fa-chevron-right arrow-icon"></i>
+                : <i className="fa-solid fa-chevron-left arrow-icon"></i>
+          }
+        </button>
+
     </>
   );
 }
